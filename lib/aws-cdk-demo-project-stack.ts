ce9260f27b3e32cfac0f6b6ecd84bae54bdc193d
Arvind Iyer <rbarvind04@gmail.com>
2024-09-25 17:27:18 -0400

    Testing bucket deployments using level 1 and level 2 constructs

--- lib/aws-cdk-demo-project-stack.ts
+++ lib/aws-cdk-demo-project-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkDemoProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    //L1 and L2 Construct of an S3 Bucket
+    const level1S3Bucket = new CfnBucket(this, 'MyFisrstLevel1ConstructBucket', {
+      versioningConfiguration: {
+        status: "Enabled"
+      }
+    });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkDemoProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const level2S3Bucket = new Bucket(this, 'MyFisrstLevel2ConstructBucket', {
+      versioned: true
+    });
   }
 }
